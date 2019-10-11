b954640d2e08e04154852bea6173bd7d10f43495
liusong <2863665257@qq.com>
2019-10-11 21:46:08 +0800

    系统管理页面

--- src/libs/menu/systemManage.js
+++ src/libs/menu/systemManage.js
@@ -16,12 +16,63 @@ export default [
             icon: "el-icon-menu",
             name: "邮件设置",
             url: 'emailSetting'
-          }
+          },
+          {
+            childResources: [],
+            icon: "el-icon-menu",
+            name: "参数设置",
+            url: 'parameterSetting'
+          },
+          {
+            childResources: [],
+            icon: "el-icon-menu",
+            name: "短信设置",
+            url: 'noteSetting'
+          },
         ],
         icon: "el-icon-menu",
         name: "系统设置",
         url: 'settings'
-      }
+      },
+      {
+        childResources: [
+          {
+            childResources: [],
+        icon: "el-icon-menu",
+        name: "数据字典",
+        url: 'dataDictionary'
+          }
+        ],
+        icon: "el-icon-menu",
+        name: "数据字典",
+        url: 'dataDictionary'
+      },
+      {
+        childResources: [
+          
+          {
+            childResources: [],
+            icon: "el-icon-menu",
+            name: "表单管理",
+            url: 'formManager'
+          },
+          {
+            childResources: [],
+            icon: "el-icon-menu",
+            name: "流程设置",
+            url: 'processSet'
+          },
+          {
+            childResources: [],
+            icon: "el-icon-menu",
+            name: "流程分类",
+            url: 'processClassify'
+          },
+        ],
+        icon: "el-icon-menu",
+        name: "业务流程",
+        url: 'businessProcess'
+      },
     ],
     icon: "el-icon-menu",
     name: "系统管理",
