aea6d3f93fce8a61c182854424bf7f50e257ff66
hudianshan <190686892@qq.com>
2019-10-11 20:53:36 +0800

    用户管理

---
+++ src/components/common/tag.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <el-tag size="medium"><i class="el-icon-info"></i><span>{{title.title}}</span>共搜索到<span class="span-item">{{title.num}}</span>条数据</el-tag>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name:'tag',
+    props:['title']
+}
+</script>
+
+<style lang="less">
+     .el-tag--medium {
+        height: 38px;
+        line-height: 38px;
+        width: 100%;
+        color: #515a6e;
+        i{
+            color: #2d8cf0;
+            padding-right:5px;
+        }
+        .span-item{
+            color:#2d8cf0;
+            padding: 0 4px;
+        }
+    }   
+</style>
---
+++ src/router/modules/roleList.js
@@ -0,0 +1,14 @@
+
+const roleList = () => import('@/views/systemManage/roleManagement/roleList')
+
+
+export default[
+    {
+        path: 'systemManage/roleList',
+        name: 'roleList',
+        meta: {
+          title: '角色列表',
+        },
+        component: roleList
+    }
+]
