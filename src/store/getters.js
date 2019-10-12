f79a8de19d9279b06586539a6797b48c20576c94
YiZH <479514106@qq.com>
2019-10-12 16:42:36 +0800

    master初始化

--- src/store/getters.js
+++
@@ -1,3 +0,0 @@
-import storage from '@/utils/storage';
-export const loginInfo = state => state.loginInfo || storage.get('loginInfo') || null
-export const entry = state => state.entry || storage.get('entry') || null
--- src/utils/httpMock.js
+++
@@ -1,4 +0,0 @@
-import Mock from 'mockjs'
-import menuData from '@/libs/menu/'
-
-Mock.mock('/api/menu', 'post', menuData);
