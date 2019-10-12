cdf09201d196de2b25297195016ca5430637fbd2
pei <alive_xin@163.com>
2019-10-12 17:36:01 +0800

    更新游戏列表

--- src/views/componentDemo/index.vue
+++ src/views/componentDemo/index.vue
@@ -46,14 +46,12 @@
         <base-info :infoList="infoList" slot="info-content"></base-info>
       </panel-edit>
     </section>
-<<<<<<< HEAD
 
     <section class="comp-item">
       <h4 class="comp-title">表格分页</h4>
       <table-paging></table-paging>
     </section>
 
-=======
     <section class="comp-item">
       <h4 class="comp-title">单行表单公共样式</h4>
       <div class="vlt-edit-single">
@@ -243,7 +241,6 @@
         </div>
       </div>
     </section>
->>>>>>> 2deaaeca23443fc419ed48367e2e205ac5acd41d
   </div>
 </template>
 
