524e7fa7c30dc298595bb9406ed9fc2fd6076f79
pei <alive_xin@163.com>
2019-10-12 20:03:21 +0800

    解决文件缺失

---
+++ src/router/modules/businessCharts.js
@@ -0,0 +1,128 @@
+// 业务监控系统路由配置
+// 中心监控
+const centerWatch = () => import('@/views/businessCharts/screenWatch/centerWatch')
+// 省级监控
+const provinceWatch = () => import('@/views/businessCharts/screenWatch/provinceWatch')
+//中央交易数据
+const centerDeal = () => import('@/views/businessCharts/dealData/centerDeal')
+//省级交易数据
+const provinceDeal = () => import('@/views/businessCharts/dealData/provinceDeal')
+//城市交易数据
+const cityDeal = () => import('@/views/businessCharts/dealData/cityDeal')
+//区域交易数据
+const areaDeal = () => import('@/views/businessCharts/dealData/areaDeal')
+//游戏交易监控
+const gameDeal = () => import('@/views/businessCharts/dealData/gameDeal')
+//销售厅账户监控
+const accountWatch = () => import('@/views/businessCharts/saleshallAccountWatch')
+//销售厅设备监控
+const equipmentWatch = () => import('@/views/businessCharts/saleshallEquipmentWatch')
+//告警监控
+const warningWatch = () => import('@/views/businessCharts/warningWatch')
+//报表管理
+const warningReport = () => import('@/views/businessCharts/warningReport')
+export default [
+  // {
+  //   path: 'demo',
+  //   name: 'demo',
+  //   component: demo,
+  //   meta: {
+  //     parentName: '', // 当左侧菜单匹配不到当前路由时(菜单无对应激活状态),请设置该属性值(菜单可激活的父级路由名称)
+  //     title: '路由说明'
+  //   }
+  // },
+  {
+    path: '/',
+    name: 'businessCharts',
+    redirect: {
+      path: 'businessCharts/centerWatch'
+    }
+  },
+  {
+    path: 'businessCharts/centerWatch',
+    name: 'centerWatch',
+    meta: {
+      title: '大屏监控',
+    },
+    component: centerWatch
+  },
+  {
+    path: 'businessCharts/provinceWatch',
+    name: 'provinceWatch',
+    meta: {
+      title: '省级监控',
+    },
+    component: provinceWatch
+  },
+
+  {
+    path: 'businessCharts/centerDeal',
+    name: 'centerDeal',
+    meta: {
+      title: '中央交易数据',
+    },
+    component: centerDeal
+  },
+  {
+    path: 'businessCharts/provinceDeal',
+    name: 'provinceDeal',
+    meta: {
+      title: '省级交易数据',
+    },
+    component: provinceDeal
+  },
+  {
+    path: 'businessCharts/cityDeal',
+    name: 'cityDeal',
+    meta: {
+      title: '市级交易数据',
+    },
+    component: cityDeal
+  },  {
+    path: 'businessCharts/areaDeal',
+    name: 'areaDeal',
+    meta: {
+      title: '区域交易数据',
+    },
+    component: areaDeal
+  },  {
+    path: 'businessCharts/gameDeal',
+    name: 'gameDeal',
+    meta: {
+      title: '游戏交易数据',
+    },
+    component: gameDeal
+  },
+  {
+    path: 'businessCharts/accountWatch',
+    name: 'accountWatch',
+    meta: {
+      title: '销售厅账户监控',
+    },
+    component: accountWatch
+  },
+  {
+    path: 'businessCharts/equipmentWatch',
+    name: 'equipmentWatch',
+    meta: {
+      title: '销售厅设备监控',
+    },
+    component: equipmentWatch
+  },{
+    path: 'businessCharts/warningWatch',
+    name: 'warningWatch',
+    meta: {
+      title: '告警监控',
+    },
+    component: warningWatch
+  },
+  {
+    path: 'businessCharts/warningReport',
+    name: 'warningReport',
+    meta: {
+      title: '告警监控',
+    },
+    component: warningReport
+  }
+
+]
