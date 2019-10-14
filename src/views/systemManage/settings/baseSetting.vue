3528893d4261d69e129d39cd33e6c342ad0d3479
liusong <2863665257@qq.com>
2019-10-14 16:18:11 +0800

    合并

--- src/views/systemManage/settings/baseSetting.vue
+++ src/views/systemManage/settings/baseSetting.vue
@@ -4,36 +4,10 @@
     <div class="vlt-edit-single">
       <div class="vlt-edit-wrap">
         <h2 class="title">用户登录</h2>
-        <el-form label-position="right" label-width="130px" :model="form" ref="form">
-          <el-form-item label="是否记住用户名">
-            <el-radio v-model="radio" label="1">是</el-radio>
-            <el-radio v-model="radio" label="2">否</el-radio>
-          </el-form-item>
-          <el-form-item label="是否保存密码">
-            <el-radio v-model="radio" label="1">是</el-radio>
-            <el-radio v-model="radio" label="2">否</el-radio>
-          </el-form-item>
-        </el-form>
+        <base-form></base-form>
+          
         <h2 class="title">记账本位币</h2>
-        <el-form label-position="right" label-width="130px" :model="form" ref="form">
-          <el-form-item label="代码">
-              <el-input v-model="form.singleAmount" placeholder="RMB"></el-input>
-            </el-form-item>
-            <el-form-item label="小数位" prop="bet" class="decimalplace">
-              <el-select v-model="form.bet" placeholder="2">
-                <el-option
-                  v-for="item in betOption"
-                  @click.native="changeBet(item)"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value"
-                ></el-option>
-              </el-select>
-            </el-form-item>
-            <el-form-item label="名称">
-              <el-input v-model="form.minMultiple" placeholder="人民币"></el-input>
-            </el-form-item>
-        </el-form>
+        <base-form ></base-form>
         <el-row class="vlt-edit-btn">
           <el-button type="primary" v-prevent="1000" size="medium" @click="save">提交</el-button>
           <el-button size="medium" @click="editShow = !editShow">取消</el-button>
