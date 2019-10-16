2e61d6112e33a169980319dcfa3c37a48768de24
LiZhengMin <you@example.com>
2019-10-16 23:26:45 +0800

    客户账户

---
+++ src/views/businessOperation/customerAccount/equityDetail.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="vlt-card">
+    <section class="comp-item">
+      <cell title="会员权益详情" style="margin-top: 10px;">
+        <base-info :infoList="infoList"></base-info>
+      </cell>
+    </section>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: "",
+  data() {
+    return {
+      infoList: [
+        { title: "权益ID", value: "", prop: "gameCode" },
+        { title: "权益名称", value: "", prop: "gameStatus" },
+        { title: "权益编码", value: "", prop: "gameName" },
+        { title: "最低级别", value: "", prop: "cycleType" },
+        { title: "权益状态", value: "", prop: "gameCode" },
+        { title: "权益描述", value: "", prop: "gameName" },
+        { title: "创建时间", value: "", prop: "officialEndSale" },
+        { title: "更新时间", value: "", prop: "gameName" }
+      ]
+    };
+  },
+  components: {},
+  methods: {}
+};
+</script>
+
+<style lang="less" scoped>
+</style>
+
