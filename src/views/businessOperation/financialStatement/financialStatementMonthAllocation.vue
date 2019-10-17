bf5afeb86d22db8442d93bf8d0eb42212e86131d
jiangbin <942887068@qq.com>
2019-10-17 17:30:34 +0800

    会员管理静态

---
+++ src/views/businessOperation/financialStatement/financialStatementMonthAllocation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="vlt-card">资金月分配报表</div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: "",
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {}
+};
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ src/views/businessOperation/financialStatement/financialStatementYearAllocation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="vlt-card">资金年分配报表</div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: "",
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {}
+};
+</script>
+
+<style lang="less" scoped>
+</style>
