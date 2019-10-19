afeb991d7ad28c82754eae2c8f5b2a737d4820e8
liusong <2863665257@qq.com>
2019-10-19 11:35:01 +0800

    switch

--- src/utils/httpMock.js
+++ src/utils/httpMock.js
@@ -39,60 +39,60 @@ Mock.mock('/api/menu', 'post', menuData);
 //       "path": '@province' + '@city' + '@county',
 //       "english": "@title",
 //       "icon": "el-icon-right",
-<<<<<<< HEAD
+// <<<<<<< HEAD
     
-=======
-<<<<<<< HEAD
+// =======
+// <<<<<<< HEAD
 
-=======
+// =======
     
->>>>>>> 4cfe348107d23c0447a85d7d0eff567536940dd5
->>>>>>> 47b24fd25180306dfb8441a1fd13fc7261f09ed9
-//       "sort": /^[1-9]$/,
-//       "date2|1": true,
-//       "date3|1-3": true
-//     },
-//     "children|10": [
-//       {
-//         "id|+1": 222,
-//         "label": "@city",
-//         "obj": {
-//           "type": "@ctitle(3,5)",
-//           "name": "@ctitle(3,5)",
-//           "path": '@province' + '@city' + '@county',
-//           "english": "@title",
-//           "icon": "el-icon-right",
-<<<<<<< HEAD
+// >>>>>>> 4cfe348107d23c0447a85d7d0eff567536940dd5
+// >>>>>>> 47b24fd25180306dfb8441a1fd13fc7261f09ed9
+// //       "sort": /^[1-9]$/,
+// //       "date2|1": true,
+// //       "date3|1-3": true
+// //     },
+// //     "children|10": [
+// //       {
+// //         "id|+1": 222,
+// //         "label": "@city",
+// //         "obj": {
+// //           "type": "@ctitle(3,5)",
+// //           "name": "@ctitle(3,5)",
+// //           "path": '@province' + '@city' + '@county',
+// //           "english": "@title",
+// //           "icon": "el-icon-right",
+// <<<<<<< HEAD
   
-=======
-<<<<<<< HEAD
+// =======
+// <<<<<<< HEAD
 
-=======
+// =======
   
->>>>>>> 4cfe348107d23c0447a85d7d0eff567536940dd5
->>>>>>> 47b24fd25180306dfb8441a1fd13fc7261f09ed9
-//           "sort": /^[1-9]$/,
-//           "date2|1": true,
-//           "date3|1-3": true
-//         },
-//         "children|15": [{
-//           "id|+1": 1111,
-//           "label": "@county",
-//           "obj": {
-//             "type": "@ctitle(3,5)",
-//             "name": "@ctitle(3,5)",
-//             "path": '@province' + '@city' + '@county',
-//             "english": "@title",
-//             "icon": "el-icon-right",
-<<<<<<< HEAD
+// >>>>>>> 4cfe348107d23c0447a85d7d0eff567536940dd5
+// >>>>>>> 47b24fd25180306dfb8441a1fd13fc7261f09ed9
+// //           "sort": /^[1-9]$/,
+// //           "date2|1": true,
+// //           "date3|1-3": true
+// //         },
+// //         "children|15": [{
+// //           "id|+1": 1111,
+// //           "label": "@county",
+// //           "obj": {
+// //             "type": "@ctitle(3,5)",
+// //             "name": "@ctitle(3,5)",
+// //             "path": '@province' + '@city' + '@county',
+// //             "english": "@title",
+// //             "icon": "el-icon-right",
+// <<<<<<< HEAD
        
-=======
-<<<<<<< HEAD
+// =======
+// <<<<<<< HEAD
 
-=======
+// =======
        
->>>>>>> 4cfe348107d23c0447a85d7d0eff567536940dd5
->>>>>>> 47b24fd25180306dfb8441a1fd13fc7261f09ed9
+// >>>>>>> 4cfe348107d23c0447a85d7d0eff567536940dd5
+// >>>>>>> 47b24fd25180306dfb8441a1fd13fc7261f09ed9
 //             "sort": /^[1-9]$/,
 //             "date2|1": true,
 //             "date3|1-3": true
@@ -199,31 +199,7 @@ Mock.mock('/api/menu', 'post', menuData);
 //   }]
 // })
 
-<<<<<<< HEAD
-Mock.mock(/\/shoopList/, "get", function (options) {
-  console.log(options);
-  // console.log(Json)
-  return data
-
-})
-
-Mock.mock(/\/menulist/, "get", function (options) {
-
-
-  // console.log(Json)
-  return data2
-
-})
-
-Mock.mock(/\/organilist/, "get", function (options) {
-
-
-  // console.log(Json)
-  return data3
-
-})
-
-=======
+// <<<<<<< HEAD
 // Mock.mock(/\/shoopList/, "get", function (options) {
 //   console.log(options);
 //   // console.log(Json)
@@ -245,10 +221,30 @@ Mock.mock(/\/organilist/, "get", function (options) {
 //   // console.log(Json)
 //   return data3
 
-<<<<<<< HEAD
-// })
-=======
 // })
 
->>>>>>> 4cfe348107d23c0447a85d7d0eff567536940dd5
->>>>>>> 47b24fd25180306dfb8441a1fd13fc7261f09ed9
+// =======
+// Mock.mock(/\/shoopList/, "get", function (options) {
+//   console.log(options);
+//   // console.log(Json)
+//   return data
+
+// })
+
+// Mock.mock(/\/menulist/, "get", function (options) {
+
+
+//   // console.log(Json)
+//   return data2
+
+// })
+
+// Mock.mock(/\/organilist/, "get", function (options) {
+
+
+//   // console.log(Json)
+//   return data3
+
+// })
+
+// >>>>>>> 47b24fd25180306dfb8441a1fd13fc7261f09ed9
