6f16f84d55e26e1736f0f334ae158136cbf764ef
liusong <2863665257@qq.com>
2019-10-22 22:53:00 +0800

    登录

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -4,52 +4,120 @@
       <img src="./../../assets/img/companyLogo.png" class="companyLogo" />
       <img src="./../../assets/img/title.png" class="title" />
       <div class="vlt-edit-single">
-        <div class="inputall">
+        <div class="inputAll">
           <base-form
-              :formData="data1"
-              labelWidth="140px"
-              ref="baseForm"
-              :rules="rules1"
-              direction="top"
-              @change="changeForm"
-            ></base-form>
+            :formData="data1"
+            labelWidth="140px"
+            ref="baseForm"
+            :rules="rules1"
+            direction="top"
+            @change="changeForm"
+          ></base-form>
         </div>
-        <div class="bottom">
-          <span class="register">注册</span>
-          <span class="forgetPwd">忘记密码</span>
+        <div class="registerPwd">
+          <!-- <span class="register">注册</span> -->
+          <el-link :underline="false" class="register">注册</el-link>
+          <!-- <span class="forgetPwd">忘记密码</span> -->
+          <el-link :underline="false" class="forgetPwd" @click="handleForgetPwd">忘记密码</el-link>
           <el-row>
             <el-button class="btn" @click="login">登录</el-button>
           </el-row>
         </div>
       </div>
     </div>
+    <div class="role-dialog">
+      <el-dialog :visible.sync="dialogFormVisible" width="600px" custom-class="roleDialog">
+        <!-- <roleifometion></roleifometion> -->
+        <div class="vlt-edit-single">
+          <h2 class="title">修改密码</h2>
+          <div class="vlt-edit-wrap">
+            <base-form
+              :formData="formData"
+              labelWidth="120px"
+              ref="baseForm"
+              :rules="rules"
+              direction="right"
+              @change="changeForm"
+            ></base-form>
+            <el-row class="vlt-edit-btn">
+              <el-button type="primary" v-prevent="1000" size="medium" @click="submit">提交并保存</el-button>
+              <el-button size="medium" @click="cancel">取消</el-button>
+            </el-row>
+          </div>
+        </div>
+      </el-dialog>
+    </div>
   </div>
 </template>
 
 <script>
-
+import rules from "../../utils/rules";
 export default {
   name: "login",
   data() {
     return {
-      input1:"",
-      input2:"",
+      dialogFormVisible: false,
+      changeForm: "",
+      input1: "",
+      input2: "",
       rules1: {},
-      data1:[
-        { type: "input", title: "", prop: "userName",value: "",placeholder:'请输入用户账号',},
-        { type: "input", title: "", prop: "pwd",value: "" ,placeholder:'请输入用户密码',},
-      ]
+      data1: [
+        {
+          type: "input-icon",
+          title: "",
+          prop: "username",
+          value: "",
+          placeholder: "请输入用户账号",
+          icon:"s-custom",
+        },
+        {
+          type: "input-icon",
+          title: "",
+          prop: "pwd",
+          value: "",
+          placeholder: "请输入用户密码",
+          icon:"lock",
+        }
+      ],
+      formData: [
+        {
+          title: "请选择你的操作",
+          type: "radio",
+          prop: "emailSend",
+          value: "",
+          options: [
+            { label: "操作密码", value: "1" },
+            { label: "登录密码", value: "0" }
+          ]
+        },
+        { title: "请输入原始登录密码", type: "password", prop: "serverSite", value: "",placeholder: "请输入" },
+        { title: "请输入新登录密码", type: "password", prop: "portNumber", value: "",placeholder: "请输入" },
+        { title: "请重复新登录密码", type: "password", prop: "userName", value: "",placeholder: "请输入"}
+      ],
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" }
+        ],
+        pwd: [{ required: true, message: "请输入密码", trigger: "blur" }]
+      }
     };
   },
   computed: {},
   created() {},
   mounted() {},
   methods: {
-    login(){
+    login() {
       this.$router.push({
-        path:'entry'
-      })
-    }
+        path: "entry"
+      });
+    },
+    handleForgetPwd() {
+      this.dialogFormVisible = true;
+    },
+    cancel() {
+      this.dialogFormVisible = false;
+    },
+    submit() {},
   },
   components: {}
 };
@@ -62,10 +130,10 @@ export default {
   background-image: url(./../../assets/img/backgroundImage.png);
   width: 100%;
   height: 100%;
-  display:flex;
+  display: flex;
   justify-content: center;
   align-items: center;
- 
+
   .mid {
     width: 490px;
     height: 520px;
@@ -86,10 +154,10 @@ export default {
       position: absolute;
       top: 242px;
       left: 60px;
-      .inputall {
+      .inputAll {
         margin-bottom: 90px;
       }
-      .bottom {
+      .registerPwd {
         .btn {
           width: 368px;
           height: 46px;
