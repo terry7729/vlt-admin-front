24c4cf7964c0b7dd5d5ab3679a80d759f1123b8b
YiZH <479514106@qq.com>
2019-10-23 16:21:33 +0800

    新增上传api,优化ajax

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -21,8 +21,17 @@ switch(process.env.VUE_APP_MODE) {
     axios.defaults.baseURL = 'http://10.7.0.91:8080/bms/api' // 本地server环境 
 }
 //http://10.6.0.103:8080/bms/api
-const request = (method, url, options) => {
-  const runRequest = async () => {
+
+/**
+* @description http请求
+* @param  {String} method 请求方法
+* @param  {String} url 请求路径
+* @param  {Object} options 请求数据
+* @param  {String} extend 请求拓展
+* @return {Function} result promise
+*/
+const request = (method, url, options, extend) => {
+  return (async () => {
     try {
       let res;
       if (typeof options !== 'object') {
@@ -30,14 +39,26 @@ const request = (method, url, options) => {
         res = await axios[method](`${url}/${id}`);
       } else {
         const data = options.data || {}
+        // 上传
+        if (extend === 'upload') {
+          res = await axios[method](url, data, {
+            // 上传进度
+            onUploadProgress: (progressEvent) => {
+              if (options.onUploadProgress) {
+                // 监听回调
+                options.onUploadProgress(progressEvent);
+              }
+             }
+          });
+          return res.data;
+        }
         res = await axios[method](url, method === 'get' ? {params: data} : data);
       }
       return res.data;
     } catch (err) {
-      console.warn('ajax-error', err);
+      console.warn('api请求错误：', err);
     }
-  };
-  return runRequest();
+  })();
 }
 
 export default {
@@ -46,6 +67,9 @@ export default {
   },
   post (url, options) {
     return request('post', url, options);
+  },
+  upload(url, options) {
+    return request('post', url, options, 'upload');
   },
   axios
 }
