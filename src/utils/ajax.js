6c7e592518e53ce588a11e2f484c79e78f2aac18
jiangbin <942887068@qq.com>
2019-10-25 22:42:31 +0800

    渠道权限账户联调完毕

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -18,14 +18,14 @@ switch (process.env.VUE_APP_MODE) {
     axios.defaults.baseURL = '//192.168.0.1/test/api'
     break
   default:
-    // axios.defaults.baseURL = 'http://10.7.0.190:8080/bms/api' // 本地server环境 http://10.7.0.91:8080/bms/api
+    axios.defaults.baseURL = 'http://10.7.0.190:8080/bms/api' // 本地server环境 http://10.7.0.91:8080/bms/api
     // axios.defaults.baseURL = 'http://10.7.0.88:8080/bms/api/vlt' // 本地server环境 
     // axios.defaults.baseURL = 'http://10.7.0.167:8080/bms/api'
     // axios.defaults.baseURL = 'http://10.7.0.87:8080/bms/api'
     // axios.defaults.baseURL = 'http://10.7.0.49:8080/bms/api'
     // axios.defaults.baseURL = 'http://10.7.0.91:8080/bms/api' // 本地server环境 
     // axios.defaults.baseURL = 'http://10.7.0.88:8080/bms/api/vlt' // 本地server环境 
-    axios.defaults.baseURL = 'http://10.6.0.103:8080/bms/api' // 本地server环境
+    //axios.defaults.baseURL = 'http://10.6.0.103:8080/bms/api' // 本地server环境
 }
 //http://10.7.0.88:8080/bms/api/vlt
 //http://10.6.0.103:8080/bms/api 测试
@@ -44,7 +44,7 @@ const request = (method, url, options, extend) => {
       let res;
       if (typeof options !== 'object') {
         const id = options;
-        res = await axios[method](`${url}/${id}`);/*  */
+        res = await axios[method](`${url}/${id}`); /*  */
       } else {
         const data = options.data || {}
         // 上传
