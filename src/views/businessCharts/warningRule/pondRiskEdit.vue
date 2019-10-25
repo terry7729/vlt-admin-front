576fcea8d25c05f01799741e5cd0594ecc8ce331
shibin <17704003982@163.com>
2019-10-25 22:10:45 +0800

    提交

--- src/views/businessCharts/warningRule/pondRiskEdit.vue
+++ src/views/businessCharts/warningRule/pondRiskEdit.vue
@@ -56,16 +56,6 @@
           </div>
         </el-form-item>
         <div class="editfrom">
-          <el-form-item prop="date2" label="监控时间点">
-            <el-time-picker
-              is-range
-              v-model="value1"
-              range-separator="至"
-              start-placeholder="开始时间"
-              end-placeholder="结束时间"
-              placeholder="选择时间范围"
-            ></el-time-picker>
-          </el-form-item>
           <el-form-item prop label="监控频率">
             <el-input-number
               v-model="form.collectFrequency"
