6e5cacc8e4ee9af8af92d7c37bc9edfda9cf91be
pei <alive_xin@163.com>
2019-10-25 09:08:30 +0800

    更新

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -26,7 +26,7 @@ switch (process.env.VUE_APP_MODE) {
     // axios.defaults.baseURL = 'http://10.7.0.49:8080/bms/api'
     // axios.defaults.baseURL = 'http://10.7.0.91:8080/bms/api' // 本地server环境 
     // axios.defaults.baseURL = 'http://10.7.0.88:8080/bms/api/vlt' // 本地server环境 
-    // axios.defaults.baseURL = 'http://10.6.0.103:8080/bms/api' // 本地server环境
+    axios.defaults.baseURL = 'http://10.6.0.103:8080/bms/api' // 本地server环境
 }
 //http://10.7.0.88:8080/bms/api/vlt
 //http://10.6.0.103:8080/bms/api 测试
