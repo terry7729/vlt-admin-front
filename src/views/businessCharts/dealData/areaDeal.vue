a5b1baa6f38a1083432955d4e6298b7e8ad54ad9
shibin <17704003982@163.com>
2019-11-01 17:32:54 +0800

    提交

--- src/views/businessCharts/dealData/areaDeal.vue
+++ src/views/businessCharts/dealData/areaDeal.vue
@@ -157,7 +157,7 @@ export default {
       const self = this;
       const res = await self.$api.getAreaDeal({
         data:options,
-        baseURL:'http://10.6.0.108:8080/api'
+        baseURL:'http://10.6.0.203:8086/api'
       });
       if (res && res.code == 0) {
         self.tableData = res.data.dataList;
