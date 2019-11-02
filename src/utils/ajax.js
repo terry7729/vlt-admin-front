fcf135a1409527d65421fa407005d0284049d53a
YiZH <479514106@qq.com>
2019-11-02 13:00:45 +0800

    更新测试环境地址

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -25,7 +25,7 @@ switch (process.env.VUE_APP_MODE) {
   default:
     // axios.defaults.baseURL = 'http://10.7.0.167:8081/bms/api'
     // axios.defaults.baseURL = 'http://10.7.0.87:8081/bms/api'
-    axios.defaults.baseURL = 'http://10.7.0.49:8081/bms/api'
+    axios.defaults.baseURL = '//10.6.0.203:8081/bms/api'
     // axios.defaults.baseURL = 'http://10.7.0.88:8081/bms/api'
     // axios.defaults.baseURL = 'http://10.7.0.190:8081/bms/api'
     // axios.defaults.baseURL = 'http://10.7.0.91:8081/bms/api'
