46cf7960e3e4ab7d157478eeaaac92dcf349ce28
Littlombie <deanwen258@outlook.com>
2019-11-04 09:15:03 +0800

    合并

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -25,15 +25,13 @@ switch (process.env.VUE_APP_MODE) {
   default:
     // axios.defaults.baseURL = 'http://10.7.0.167:8081/bms/api'
     // axios.defaults.baseURL = 'http://10.7.0.87:8081/bms/api'
-    axios.defaults.baseURL = 'http://10.7.0.49:8081/bms/api'
+    // axios.defaults.baseURL = 'http://10.6.0.203:8888/bms/api'
     // axios.defaults.baseURL = 'http://10.7.0.88:8081/bms/api'
     // axios.defaults.baseURL = 'http://10.7.0.190:8081/bms/api'
     // axios.defaults.baseURL = 'http://10.7.0.91:8081/bms/api'
     // axios.defaults.baseURL = 'http://10.7.0.89:8081/bms/api'
     // axios.defaults.baseURL = 'http://10.7.0.103:8081/bms/api'
     // axios.defaults.baseURL = 'http://10.7.0.89:8081/bms/api'
-
-
 }
 /**
  * @description http请求
